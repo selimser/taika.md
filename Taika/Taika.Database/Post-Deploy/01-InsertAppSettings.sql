4c51f8e4e049c46fab280d021f2cb7f6ebaea8a6
Selim S. Balci <16270624+selimser@users.noreply.github.com>
2020-10-09 19:19:37 +0100

    Improving the app settings post deployment script, so that it only inserts the missing default keys, without destroying the user's other keys.

--- Taika/Taika.Database/Post-Deploy/01-InsertAppSettings.sql
+++ Taika/Taika.Database/Post-Deploy/01-InsertAppSettings.sql
@@ -1,4 +1,4 @@
-﻿
+﻿SET NOCOUNT ON
 
 DECLARE @DefaultAppSettings TABLE
 (
@@ -11,24 +11,39 @@ VALUES
     ('Theme', 'default')
 
 BEGIN TRY
+    PRINT 'Inserting missing default application settings'
     BEGIN TRANSACTION
-        INSERT INTO [dbo].[ApplicationSettings]
-        (
-            [Key],
-            [Value],
-            [CreatedOn]
-        )
+        INSERT INTO [dbo].[ApplicationSettings] ([Key], [Value])
         SELECT 
-            [Key],
-            [Value],
-            GETUTCDATE()
-        FROM @DefaultAppSettings
-    COMMIT TRANSACTION
+	        DAS.[Key], 
+	        DAS.[Value]
+        FROM dbo.ApplicationSettings AS APS WITH (READCOMMITTED)
+        RIGHT JOIN @DefaultAppSettings AS DAS
+        ON APS.[Key] = DAS.[Key]
+        WHERE APS.[Key] IS NULL
+
+		DECLARE @RecordCount SMALLINT = @@ROWCOUNT
+
+		PRINT 'Default application settings creation completed!'
+    
+		IF @RecordCount <= 0
+		BEGIN
+			PRINT 'No new settings were inserted.'
+		END
+		ELSE
+		BEGIN
+			PRINT CONCAT(@RecordCount, ' new settings were created.')
+		END
+	COMMIT TRANSACTION
 END TRY
 BEGIN CATCH
+	ROLLBACK TRANSACTION
+
     DECLARE @ErrorMessage VARCHAR(300) 
         = CONCAT('Error inserting default application settings', ERROR_MESSAGE())
 
     PRINT 'ERROR!!!'
     RAISERROR(@ErrorMessage, 5, 100)
 END CATCH
+
+SET NOCOUNT OFF
