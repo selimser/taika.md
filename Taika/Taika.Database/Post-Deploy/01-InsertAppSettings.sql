07f8f7c457e910899e4326cc2c84f9f97d71b533
Selim S. Balci <16270624+selimser@users.noreply.github.com>
2020-10-09 18:52:13 +0100

    Table and column name changes, post deployment script for default app settings.

---
+++ Taika/Taika.Database/Post-Deploy/01-InsertAppSettings.sql
@@ -0,0 +1,34 @@
+﻿
+
+DECLARE @DefaultAppSettings TABLE
+(
+    [Key] VARCHAR(30),
+    [Value] VARCHAR(100)
+)
+
+INSERT INTO @DefaultAppSettings([Key], [Value])
+VALUES
+    ('Theme', 'default')
+
+BEGIN TRY
+    BEGIN TRANSACTION
+        INSERT INTO [dbo].[ApplicationSettings]
+        (
+            [Key],
+            [Value],
+            [CreatedOn]
+        )
+        SELECT 
+            [Key],
+            [Value],
+            GETUTCDATE()
+        FROM @DefaultAppSettings
+    COMMIT TRANSACTION
+END TRY
+BEGIN CATCH
+    DECLARE @ErrorMessage VARCHAR(300) 
+        = CONCAT('Error inserting default application settings', ERROR_MESSAGE())
+
+    PRINT 'ERROR!!!'
+    RAISERROR(@ErrorMessage, 5, 100)
+END CATCH
